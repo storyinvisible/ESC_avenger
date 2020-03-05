eabc1e215f5d1a3e31d6d7d6838395e84fc23bcc
Zhibo <storyinvisible@gmail.com>
2020-03-05 21:10:10 +0800

    revert to old ways of scustomer sign in

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ var user_credentials = require("./rainbow_server");
 app.use(express.static('SDKAngularSample'));
 app.use(bodyParser.json());
 
-/*
+
 var user_detail={
     user: "some@email.com",
     password:"aPassword_123",
-} */
+} 
 app.post('/message', function(req, res){
     var obj={};
     console.log('body :'+ JSON.stringify(req.body));
-    res.send(user_credentials.credentials);
+    res.send(user_detail);
 })
 app.listen(3001);
