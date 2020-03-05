585dff0ac7025312b83f5f22cb7666056b86e2ea
Wilson Koh <NikoKoh98@gmail.com>
2020-03-05 20:31:47 +0800

    rainbow server

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var app = express();
 var bodyParser = require('body-parser');
 var guest_user='';
 var guest_password=''
+
+var user_credentials = require("./rainbow_server");
+
 app.use(express.static('SDKAngularSample'));
 app.use(bodyParser.json());
+
+/*
 var user_detail={
     user: "some@email.com",
     password:"aPassword_123",
-}
+} */
 app.post('/message', function(req, res){
     var obj={};
     console.log('body :'+ JSON.stringify(req.body));
-    res.send(user_detail);
+    res.send(user_credentials.credentials);
 })
 app.listen(3001);
